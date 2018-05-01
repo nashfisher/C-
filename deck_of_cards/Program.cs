60d04f56bbeb89261632a50afd082e1664a3016f
nashfisher <nashfisher1991@gmail.com>
2018-05-01 15:02:29 -0500

    update

---
+++ deck_of_cards/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deck_of_cards
+{
+
+    public class Card {
+
+        public string[] stringVal = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        public string[] suit = {"Clubs", "Spades", "Hearts", "Diamonds"};
+
+    }
+    public class Deck:Card {
+
+        public List<string> cards = new List<string>{};
+        public Random rand = new Random();
+
+        public void cardCreate() {
+            for ( int i = 0; i < this.suit.Length; i++) {
+                for (int j = 0; j < this.stringVal.Length; j++) {
+                    this.cards.Add(this.suit[i] + " " + this.stringVal[j]);
+                }
+            }
+        }
+
+        public string Deal() {
+            // cardCreate();
+            string card = this.cards[this.cards.Count-1];
+            this.cards.RemoveAt(this.cards.Count-1);
+            return card;
+        }
+        public void Shuffle(){
+            for(int i = 0; i < this.cards.Count; i++){
+                int index = rand.Next(0,this.cards.Count - 1);
+                string temp = this.cards[i];
+                this.cards[i] = this.cards[index];
+                this.cards[index] = temp;
+            }
+        }
+        public void Reset(){
+            cardCreate();
+        }
+
+
+    }
+
+    public class Player {
+        public string name;
+        public List<string> Hand = new List<string>{};
+
+        public string Draw(List<string> a) {
+            this.Hand.Add(a[a.Count - 1]);
+            Console.WriteLine(a[a.Count - 1]);
+            return a[a.Count - 1];
+        }
+        public string Discard(string card){
+            if(this.Hand.Contains(card)){
+                this.Hand.Remove(card);
+                return "removed";
+            }
+            else {
+                return $"You don't have {card}";
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Deck cardDeck = new Deck();
+            Player Tomas = new Player();
+            cardDeck.cardCreate();
+            cardDeck.Shuffle();
+            Tomas.Draw(cardDeck.cards);
+            Console.WriteLine(Tomas.Discard("Spades 4"));
+        }
+    }
+}
---
+++ fundamentals1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Print 1-255
+            for (int i = 1; i <= 255; i++) {
+                Console.WriteLine(i);
+            }
+
+            // Print multiples of 3 and 5 from 1-100 with modulus
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+            }
+
+            // Modulus without modulus
+            int i = 1;
+            while (i <= 100) {
+                // like this
+                if (i/15 == i/15.0) {
+                    Console.WriteLine("FizzBuzz");
+                    i = i + 1;
+                }
+                // or this
+                if (i/3 == (double)i/3) {
+                    Console.WriteLine("Fizz");
+                    i = i + 1;
+                }
+                if (i/5 == (double)i/5) {
+                    Console.WriteLine("Buzz");
+                    i = i + 1;
+                }
+                Console.WriteLine(i);
+                i = i + 1;
+            }
+
+            // Generate 10 random 'FizzBuzz' values
+            Random rand = new Random();
+            int x = 1;
+            while (x <= 10) {
+                int word = rand.Next(1,100);
+                if (word % 15 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                    x++;
+                }
+                else if (word % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                    x++;
+                }
+                else if (word % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                    x++;
+                }              
+                Console.WriteLine(word);
+                x++;
+            }
+
+        }
+    }
+}
